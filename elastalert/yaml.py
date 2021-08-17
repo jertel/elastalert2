e98827cb44e10c8e9e89f571e34de48a34153681
Xeph <iamxeph@gmail.com>
2021-08-17 13:21:54 +0000

    use pyyaml-include

--- elastalert/yaml.py
+++ elastalert/yaml.py
@@ -1,6 +1,8 @@
 import os
 import yaml
+from yamlinclude import YamlIncludeConstructor
 
+YamlIncludeConstructor.add_to_loader_class(loader_class=yaml.FullLoader, base_dir='/opt/elastalert')
 
 def read_yaml(path):
     with open(path) as f:
