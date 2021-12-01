ce2438cc06c726655f2b4787439fb9649e385c99
Jason Ertel <jason.ertel@securityonionsolutions.com>
2021-12-01 08:27:16 -0500

    Initial helm chart repo

--- docs/source/recipes/faq.rst
+++
@@ -1,6 +0,0 @@
-.. _faq:
-
-Frequently Asked Questions
---------------------------
-
-.. mdinclude:: faq-md.md
--- elastalert/yaml.py
+++
@@ -1,8 +0,0 @@
-import os
-import yaml
-
-
-def read_yaml(path):
-    with open(path) as f:
-        yamlContent = os.path.expandvars(f.read())
-        return yaml.load(yamlContent, Loader=yaml.FullLoader)
