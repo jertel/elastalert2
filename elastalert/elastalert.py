982f592cba96b6e17a0109ab68142effaab9859b
Jason Ertel <github@jasonertel.com>
2023-06-08 21:21:44 -0400

    add log line when exiting due to --end param falling in the past

--- elastalert/elastalert.py
+++ elastalert/elastalert.py
@@ -1132,7 +1132,9 @@ class ElastAlerter(object):
             if self.args.end:
                 endtime = ts_to_dt(self.args.end)
 
-                if next_run.replace(tzinfo=dateutil.tz.tzutc()) > endtime:
+                dt = next_run.replace(tzinfo=dateutil.tz.tzutc())
+                if dt > endtime:
+                    elastalert_logger.info("End time '%s' falls before the next run time '%s', exiting." % (endtime, dt))
                     exit(0)
 
             if next_run < datetime.datetime.utcnow():
