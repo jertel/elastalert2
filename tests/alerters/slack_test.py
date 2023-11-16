6dfe30858da99212d39d5301076747e3c1d9044c
luffynextgen <34096485+luffynextgen@users.noreply.github.com>
2023-11-16 09:07:02 +0100

    Update slack_test.py

--- tests/alerters/slack_test.py
+++ tests/alerters/slack_test.py
@@ -455,6 +455,7 @@ def test_slack_kibana_discover_color():
     actual_data = json.loads(mock_post_request.call_args_list[0][1]['data'])
     assert expected_data == actual_data
 
+
 def test_slack_attach_opensearch_discover_url_when_generated():
     rule = {
         'name': 'Test Rule',
