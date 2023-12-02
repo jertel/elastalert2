4a04597f4592a3558f0c25db4826db857334965c
Jason Ertel <github@jasonertel.com>
2023-12-02 16:18:09 -0500

    upgrade to Python 3.12

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         "Discussion Forum": "https://github.com/jertel/elastalert2/discussions",
     },
     classifiers=[
-        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
     ],
