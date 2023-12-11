985df30c259406190f4c72f12007c32922ea83d4
James Macdonell <jmacdone@csusb.edu>
2023-12-10 19:16:15 -0800

    hits_terms_test - parameterized senarios

--- tests/hits_terms_test.py
+++ tests/hits_terms_test.py
@@ -29,11 +29,70 @@ def example_agg_response():
     return res
 
 
-def test_query_key_filter_happy_path(ea, example_agg_response):
+def _mock_query_key_option_loader(rule):
+    '''
+    So, some copypasta from loaders.load_options,
 
-    ea.rules[0]['compound_query_key'] = ['server_ip', 'service_name']
-    qk = ('172.16.1.10', '/api/v1/endpoint-foo')
-    qk_csv = ", ".join(qk)
+    if query_key is a string:
+        no compound_query_key is created
+    if query_key is a list:
+        if len() > 1:
+            compound_query_key is created
+            query_key is replaced with ",".join() of the original query_key values
+        if len() == 1:
+            the query_key list with one string is normalilzed back to just a string
+        if len() == 0:
+            somehow it was an empty list and query_keys is silently dropped from the config
+    '''
+    raw_query_key = rule.get('query_key')
+    if isinstance(raw_query_key, list):
+        if len(raw_query_key) > 1:
+            rule['compound_query_key'] = raw_query_key
+            rule['query_key'] = ','.join(raw_query_key)
+        elif len(raw_query_key) == 1:
+            rule['query_key'] = raw_query_key[0]
+        else:
+            del(rule['query_key'])
+
+@pytest.mark.parametrize(
+    ["qk", "query_key"],
+
+    #scenario A: 3 query keys
+    [ ( ['172.16.1.10', '/api/v1/endpoint-foo', 'us-east-2'],
+        ['server_ip', 'service_name', 'region'] ),
+
+    #scenario B: 2 query keys
+      ( ['172.16.1.10', '/api/v1/endpoint-foo'],
+        ['server_ip', 'service_name'] ),
+
+    #scenario C: 1 query key, but it was given as a list of one fieldname in the rule options
+      ( ['172.16.1.10'],
+        ['server_ip'] ),
+
+    #scenario D: 1 query key, given as a string
+      ( ['172.16.1.10'],
+        'server_ip' ),
+
+    #scenario E: no query key
+      ( None,
+        None )
+   ],
+)
+def test_query_key_filter_happy_path(ea, example_agg_response, qk, query_key):
+
+    if query_key is not None:
+        ea.rules[0]['query_key'] = query_key
+
+    # emulate the rule['compound_query_key'] creation logic which prob should be
+    # factored out of loaders.load_options() instead of copypasta'd for the test
+    _mock_query_key_option_loader(ea.rules[0])
+
+    try:
+        # todo: why are these values being passed as a commaspace-join and not a
+        # comma-join like everything else?
+        qk_csv = ", ".join(qk)
+    except TypeError:
+        qk_csv = None
     index = 'foo-2023-13-13' #lousy Smarch weather
     top_term_key = 'client_ip'
 
@@ -54,12 +113,22 @@ def test_query_key_filter_happy_path(ea, example_agg_response):
     assert hit_terms[endtime] == example_agg_response['aggregations']['counts']['buckets']
 
     expected_filters = [
-        {'range': {'@timestamp': {
-            'gt': dt_to_ts(starttime),
-            'lte': dt_to_ts(endtime)}}},
-        {'term': {f'{ea.rules[0]['compound_query_key'][0]}.keyword': qk[0]}},
-        {'term': {f'{ea.rules[0]['compound_query_key'][1]}.keyword': qk[1]}}
+        {'range': {'@timestamp': { 'gt': dt_to_ts(starttime), 'lte': dt_to_ts(endtime)}}}
     ]
+    try:
+        cqk = ea.rules[0]['compound_query_key']
+        for fieldname, value in zip(cqk, qk):
+            filter = {'term': {f'{fieldname}.keyword': value}}
+            expected_filters.append(filter)
+    except KeyError:
+        #not a compound, eh?  it must be a string of a single filedname
+        try:
+            fieldname = ea.rules[0]['query_key']
+            filter = {'term': {f'{fieldname}.keyword': qk[0]}}
+            expected_filters.append(filter)
+        except KeyError:
+            pass # maybe the rule never had a query_key, or it was an empty list and purged
+
     expected_query = {
         'query': {'bool': {'filter': {'bool': {'must': expected_filters}}}},
          # 50 harded coded in get_hits_terms as a default for size=None
