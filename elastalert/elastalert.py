a1bd4bde34ddcb44b23cd7a2466e9f4539f103f6
kormakov.n <kormakov.n@obelis.ltd>
2025-08-20 17:19:43 +0300

    prevent from exit when running back in time

--- elastalert/elastalert.py
+++ elastalert/elastalert.py
@@ -1171,8 +1171,8 @@ class ElastAlerter(object):
 
                 next_run_dt = next_run.replace(tzinfo=datetime.timezone.utc)
                 if next_run_dt > endtime:
-                    elastalert_logger.info("End time '%s' falls before the next run time '%s', exiting." % (endtime, next_run_dt))
-                    exit(0)
+                    elastalert_logger.info("End time '%s' falls before the next run time '%s', all rules will be cheked once." % (endtime, next_run_dt))
+                    self.running = False
 
             if next_run < datetime.datetime.now(tz=datetime.UTC):
                 continue
